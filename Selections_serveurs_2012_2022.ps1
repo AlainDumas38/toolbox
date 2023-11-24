efdbac0a28c9bb7bab461517b7521e6b0f90fc4c
Alain Dumas <alain.dumas@capgemini.com>
2023-11-24 16:07:30 +0100

    nouveau code ajouté dans un git flow il permet de selctionner des Selections_serveurs_2012_2022

---
+++ Selections_serveurs_2012_2022.ps1
@@ -0,0 +1,41 @@
+﻿# Saisissez le nom du domaine
+$domainName = Read-Host "Entrez le nom du domaine (par exemple : mondomaine.local)"
+
+# Obtenez la liste des serveurs Windows 2012, 2016, 2019 et 2022 dans le domaine
+$servers = Get-ADComputer -Filter {((OperatingSystem -like "Windows Server 2012*") -or
+                                    (OperatingSystem -like "Windows Server 2016*") -or
+                                    (OperatingSystem -like "Windows Server 2019*") -or
+                                    (OperatingSystem -like "Windows Server 2022*")) -and
+                                   (Enabled -eq $true)} -Server $domainName | Select-Object -ExpandProperty Name
+
+# Vérifiez s'il y a des serveurs Windows dans le domaine
+if ($servers.Count -eq 0) {
+    Write-Host "Aucun serveur Windows trouvé dans le domaine."
+} else {
+    # Demandez à l'utilisateur de saisir le préfixe du nom de serveur
+    $prefix = Read-Host "Entrez le préfixe du nom de serveur (par exemple : ADK pour les serveurs qui commencent par ADK)"
+    
+    # Filtrer les serveurs en fonction du préfixe
+    $filteredServers = $servers | Where-Object { $_ -like "$prefix*" }
+
+    if ($filteredServers.Count -eq 0) {
+        Write-Host "Aucun serveur ne correspond au préfixe '$prefix'."
+    } else {
+        # Ouvrez une boîte de dialogue pour enregistrer le fichier texte
+        $saveFileDialog = New-Object System.Windows.Forms.SaveFileDialog
+        $saveFileDialog.Filter = "Fichiers texte (*.txt)|*.txt"
+        $saveFileDialog.Title = "Enregistrer la liste des serveurs Windows"
+        $saveFileDialog.InitialDirectory = [Environment]::GetFolderPath("MyDocuments")
+
+        if ($saveFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+            $destinationPath = $saveFileDialog.FileName
+
+            # Écrivez la liste des serveurs Windows filtrés dans le fichier texte de destination
+            $filteredServers | Out-File -FilePath $destinationPath
+
+            Write-Host "Liste des serveurs Windows enregistrée dans $destinationPath."
+        } else {
+            Write-Host "Aucun fichier n'a été enregistré."
+        }
+    }
+}
